7a7e0cc10869ff1a24e0e0662af8d38520985c9f
KhomDV <KhomDV@mail.ru>
2021-03-18 22:58:30 +0300

    refactor: start task hanoi-tower

--- basic-js.js
+++ basic-js.js
@@ -232,46 +232,46 @@
 //Напишите свой код в `src/extended-repeater.js`.
 //
 //---Solution---
-function repeater(str, options) {
-    let sRepeater = "";
-    let addStr = "";
-
-    let repeatTimes = "repeatTimes" in options ? options.repeatTimes : (str === "" ? 0: 1);
-    let separator = "separator" in options ? options.separator : '+';
-    let addition = "addition" in options ? options.addition : "";
-    let additionRepeatTimes = "additionRepeatTimes" in options ? options.additionRepeatTimes : (addition === "" ? 0 : 1);
-    let additionSeparator = "additionSeparator" in options ? options.additionSeparator : '|';
-
-    for (i=1;i<=repeatTimes;i++) {
-        addStr = "";
-        for (j=1;j<=additionRepeatTimes;j++) {
-            addStr = addStr.concat((addStr !== "" ? additionSeparator : ""), addition);
-        }    
-        sRepeater = sRepeater.concat((sRepeater !== "" ? separator : ""), str, addStr);
-    }
-
-    return (sRepeater);
-}
-
+// function repeater(str, options) {
+//     let sRepeater = "";
+//     let addStr = "";
+//
+//     let repeatTimes = "repeatTimes" in options ? options.repeatTimes : (str === "" ? 0: 1);
+//     let separator = "separator" in options ? options.separator : '+';
+//     let addition = "addition" in options ? options.addition : "";
+//     let additionRepeatTimes = "additionRepeatTimes" in options ? options.additionRepeatTimes : (addition === "" ? 0 : 1);
+//     let additionSeparator = "additionSeparator" in options ? options.additionSeparator : '|';
+//
+//     for (i=1;i<=repeatTimes;i++) {
+//         addStr = "";
+//         for (j=1;j<=additionRepeatTimes;j++) {
+//             addStr = addStr.concat((addStr !== "" ? additionSeparator : ""), addition);
+//         }    
+//         sRepeater = sRepeater.concat((sRepeater !== "" ? separator : ""), str, addStr);
+//     }
+//
+//     return (sRepeater);
+// }
+//
 //---Test---
-let arrayTest = [
-    ['TESTstr', { separator: 'ds', addition: 'ADD!', additionSeparator: ')))000' }], //'TESTstrADD!');
-    [9.234, { repeatTimes: 4, separator: '||', addition: {a: 5}, additionRepeatTimes: 3, additionSeparator: '&&' }], // '9.234[object Object]&&[object Object]&&[object Object]||9.234[object Object]&&[object Object]&&[object Object]||9.234[object Object]&&[object Object]&&[object Object]||9.234[object Object]&&[object Object]&&[object Object]')
-    [-222, { repeatTimes: 4, separator: '||', addition: new Map(), additionRepeatTimes: 3, additionSeparator: '&&' }], //'-222[object Map]&&[object Map]&&[object Map]||-222[object Map]&&[object Map]&&[object Map]||-222[object Map]&&[object Map]&&[object Map]||-222[object Map]&&[object Map]&&[object Map]');
-    [new Set(), { repeatTimes: 3, separator: '??? ', addition: [1, 2, 3, '4'], additionRepeatTimes: 2, additionSeparator: '!!!' }], //'[object Set]1,2,3,4!!!1,2,3,4??? [object Set]1,2,3,4!!!1,2,3,4??? [object Set]1,2,3,4!!!1,2,3,4');
-    [true, { repeatTimes: 3, separator: '??? ', addition: false, additionRepeatTimes: 2, additionSeparator: '!!!' }], //'truefalse!!!false??? truefalse!!!false??? truefalse!!!false');
-    [null, { repeatTimes: 3, separator: '??? ', addition: null, additionRepeatTimes: 3, additionSeparator: '!!!' }], //'nullnull!!!null!!!null??? nullnull!!!null!!!null??? nullnull!!!null!!!null');  
-    ['REPEATABLE_STRING', {repeatTimes: 2, addition: 'ADDITION', additionRepeatTimes: 3}], //'REPEATABLE_STRINGADDITION|ADDITION|ADDITION+REPEATABLE_STRINGADDITION|ADDITION|ADDITION');
-    ['REPEATABLE_STRING', {repeatTimes: 2, addition: 'ADDITION', additionSeparator: '222', additionRepeatTimes: 3}], //'REPEATABLE_STRINGADDITION222ADDITION222ADDITION+REPEATABLE_STRINGADDITION222ADDITION222ADDITION');
-    ['REPEATABLE_STRING', {repeatTimes: 2, separator: '222', addition: 'ADDITION', additionRepeatTimes: 3}] // 'REPEATABLE_STRINGADDITION|ADDITION|ADDITION222REPEATABLE_STRINGADDITION|ADDITION|ADDITION');
-]
+// let arrayTest = [
+//     ['TESTstr', { separator: 'ds', addition: 'ADD!', additionSeparator: ')))000' }], //'TESTstrADD!');
+//     [9.234, { repeatTimes: 4, separator: '||', addition: {a: 5}, additionRepeatTimes: 3, additionSeparator: '&&' }], // '9.234[object Object]&&[object Object]&&[object Object]||9.234[object Object]&&[object Object]&&[object Object]||9.234[object Object]&&[object Object]&&[object Object]||9.234[object Object]&&[object Object]&&[object Object]')
+//     [-222, { repeatTimes: 4, separator: '||', addition: new Map(), additionRepeatTimes: 3, additionSeparator: '&&' }], //'-222[object Map]&&[object Map]&&[object Map]||-222[object Map]&&[object Map]&&[object Map]||-222[object Map]&&[object Map]&&[object Map]||-222[object Map]&&[object Map]&&[object Map]');
+//     [new Set(), { repeatTimes: 3, separator: '??? ', addition: [1, 2, 3, '4'], additionRepeatTimes: 2, additionSeparator: '!!!' }], //'[object Set]1,2,3,4!!!1,2,3,4??? [object Set]1,2,3,4!!!1,2,3,4??? [object Set]1,2,3,4!!!1,2,3,4');
+//     [true, { repeatTimes: 3, separator: '??? ', addition: false, additionRepeatTimes: 2, additionSeparator: '!!!' }], //'truefalse!!!false??? truefalse!!!false??? truefalse!!!false');
+//     [null, { repeatTimes: 3, separator: '??? ', addition: null, additionRepeatTimes: 3, additionSeparator: '!!!' }], //'nullnull!!!null!!!null??? nullnull!!!null!!!null??? nullnull!!!null!!!null');  
+//     ['REPEATABLE_STRING', {repeatTimes: 2, addition: 'ADDITION', additionRepeatTimes: 3}], //'REPEATABLE_STRINGADDITION|ADDITION|ADDITION+REPEATABLE_STRINGADDITION|ADDITION|ADDITION');
+//     ['REPEATABLE_STRING', {repeatTimes: 2, addition: 'ADDITION', additionSeparator: '222', additionRepeatTimes: 3}], //'REPEATABLE_STRINGADDITION222ADDITION222ADDITION+REPEATABLE_STRINGADDITION222ADDITION222ADDITION');
+//     ['REPEATABLE_STRING', {repeatTimes: 2, separator: '222', addition: 'ADDITION', additionRepeatTimes: 3}] // 'REPEATABLE_STRINGADDITION|ADDITION|ADDITION222REPEATABLE_STRINGADDITION|ADDITION|ADDITION');
+// ]
 //
 //---View solution---
-for (let i=0;i<arrayTest.length;i++) {
-    let sRepeate = repeater(arrayTest[i][0],arrayTest[i][1]);
-    document.write(sRepeate);
-    document.write('-');
-}
+// for (let i=0;i<arrayTest.length;i++) {
+//     let sRepeate = repeater(arrayTest[i][0],arrayTest[i][1]);
+//     document.write(sRepeate);
+//     document.write('-');
+// }
 //
 //const objWithSpecificCoercion = {
 //    [Symbol.toPrimitive]: hint => hint !== 'number' ? 'STRING_OR_DEFAULT' : 'NUMBER'
@@ -280,4 +280,71 @@ for (let i=0;i<arrayTest.length;i++) {
 //document.write(sRepeate);
 //
 //=== End ( extended-repeater ) ===
+
+
+//---------------------------------------------------------------------------------------------------
+// hanoi-tower
+//---------------------------------------------------------------------------------------------------
+//---Task---
+//### **Ханойская башня**
+//
+//![Визуализация алгоритма](https://ioecapsule.com/wp-content/uploads/2019/08/tower_of_hanoi_3_disks.gif)
+//
+// [Ханойская башня](https://www.britannica.com/topic/Tower-of-Hanoi) — знаменитая математическая головоломка 18 столетия.
+// Она состоит из трех стержней и некоторого числа дисков разных размеров, которые могут быть надеты на стержень.
+// Головоломка начинается с того, что диски расположены друг на друге, причем наименьший расположен сверху.
+// Диски образуют конус.
+// Цель головоломки — переместить всю стопку на другой стержень, следуя этим простым **правилам**:
+// * перемещать можно только **один** диск за раз
+// * можно брать только **верхний** диск с одной из стопок и помещать на **верхушку** другой стопки или на пустой стержень
+// * диск **большего** размера нельзя класть на диск **меньшего** размера
+// Ваша задача значительно легче, чем придумывать алгоритм, решающий эту задачу :)
+// Реализуйте функцию `calculateHanoi`, которая принимает параметры `diskNumber` и `turnsSpeed`.
+//  `diskNumber` — это число **дисков**, а `turnsSpeed` — скорость перемещения дисков (в **ходах** в **час**).
+//   Оба параметра являются числами (тип `number`)
+// Функция `calculateHanoi` возвращает объект с 2 свойствами:
+// * `turns` (минимальное число (тип `number`) ходов, необходимое для решения головоломки)
+// * `seconds` (минимальное число (тип `number`) **секунд**, необходимое для решения головоломки при заданной скорости;
+//  должно быть целым числом, полученным в результате округления результата расчетов в меньшую **(floor)** сторону)
+// Вам не нужно валидировать входные параметры.
+// Например:
+// `calculateHanoi(9, 4308) => { turns: 511, seconds: 427 }`
+// Напишите ваш код в `src/hanoi-tower.js`.
+// ---
+//
+//---Solution---
+function calculateHanoi(disksNumber, turnsSpeed) {
+
+};
+
+//---Test---
+let arrayTest = [
+    [5, 4074],  //{turns: 31, seconds: 27});
+    [38, 4594], //{turns: 274877906943, seconds: 215402800390});
+    [34, 4005], //{turns: 17179869183, seconds: 15442579040});
+    [19, 4770], //{turns: 524287, seconds: 395688});
+    [23, 4344], //{turns: 8388607, seconds: 6951884});
+    [9, 4347],  //{turns: 511, seconds: 423});
+    [15, 4254], //{turns: 32767, seconds: 27729});
+    [14, 4681], //{turns: 16383, seconds: 12599});
+    [9, 4308],  //{turns: 511, seconds: 427});
+    [18, 4442], //{turns: 262143, seconds: 212452});
+    [10, 4159], //{turns: 1023, seconds: 885});
+    [44, 4685], //{turns: 17592186044415, seconds: 13518008486636});
+    [39, 4936], //{turns: 549755813887, seconds: 400956428280});
+    [44, 4468], //{turns: 17592186044415, seconds: 14174545604273});
+    [46, 4236]  //{turns: 70368744177663, seconds: 59803465306795});
+]
+//
+//---View solution---
+for (let i=0;i<arrayTest.length;i++) {
+    let objHanoi = calculateHanoi(arrayTest[i][0],arrayTest[i][1]);
+    document.write(objHanoi);
+    document.write('-');
+}
+//
+//let objHanoi = calculateHanoi(5, 4074); //{turns: 31, seconds: 27})
+//document.write(objHanoi);
+// 
+//=== End ( hanoi-tower ) ===
 
