1ac60078c411b7ef300d6417d1216226a4e464a0
KhomDV <KhomDV@mail.ru>
2021-03-24 00:17:22 +0300

    refactor: edit vigenere-cipher

--- basic-js.js
+++ basic-js.js
@@ -635,48 +635,59 @@ class VigenereCipheringMachine {
         if ( !message || !key) throw new Error();
 
         let strCrypto = "";
-
-
-        let ap = [];
+        let i_key = 0;
 
         for (let i=0; i<message.length; i++) {
             const char = message[i].toUpperCase();
-
             if (char.charCodeAt(0) >= 65 && char.charCodeAt(0) <= 90) {
-                const i_key = 0;
-                if (message.length > key.length) {
-                    ap.push(message.length % key.length);
-                    //i_key = message.length % key.length;
-                }
+              const charKey = key[i_key].toUpperCase();
+              let charCode = (char.charCodeAt(0)-65) + (charKey.charCodeAt(0)-65);
+              if (Math.trunc(charCode / 25) > 0 ) {
+                charCode = (charCode % 25)-1;
+              }
+              strCrypto = strCrypto + String.fromCharCode(charCode + 65);
+              i_key++;
+              if (i_key > key.length-1) {
+                i_key = 0;
+              }
             } else {
-                strCrypto = strCrypto.concat(char);
+              strCrypto = strCrypto + message[i];
             }
-
-            Math.trunc();
-
-            //toUpperCase()
-
         }
-
-        // if (message.length > key.length) {
-        //     strKey
-
-        // }
-        // strKey
-
-
-        // 'ABC'.charCodeAt(0);65
-
-
-        return (ap);
+        if (!this.lineCrypt) {
+          strCrypto.split().reverse.join('');
+        }
+        return (strCrypto);
     }
 
 
     decrypt(message, key) {
         if ( !message || !key) throw new Error();
-        return 'Z'.charCodeAt(0);
 
-        return ("d");
+        let strDeCrypto = "";
+        let i_key = 0;
+
+        for (let i=0; i<message.length; i++) {
+            const charCrypto = message[i].toUpperCase();
+            if (charCrypto.charCodeAt(0) >= 65 && charCrypto.charCodeAt(0) <= 90) {
+              const charKey = key[i_key].toUpperCase();
+              let charCode = (charCrypto.charCodeAt(0)-65) - (charKey.charCodeAt(0)-65);
+              if ( charCode < 0 ) {
+                charCode = 26 + (charCrypto.charCodeAt(0)-65) - (charKey.charCodeAt(0)-65);
+              }
+              strDeCrypto = strDeCrypto + String.fromCharCode(charCode + 65);
+              i_key++;
+              if (i_key > key.length-1) {
+                i_key = 0;
+              }
+            } else {
+              strDeCrypto = strDeCrypto + message[i];
+            }
+        }
+        if (!this.lineCrypt) {
+          strDeCrypto.split().reverse.join('');
+        }
+        return (strDeCrypto);
     }
 
 }
