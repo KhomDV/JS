a91192885ebacc7c44cbec1e60074f4af9b6e979
KhomDV <khomdv@mail.ru>
2021-03-24 09:54:40 +0300

    feat: solution vigenere-cipher

--- basic-js.js
+++ basic-js.js
@@ -638,26 +638,23 @@ class VigenereCipheringMachine {
         let i_key = 0;
 
         for (let i=0; i<message.length; i++) {
-            const char = message[i].toUpperCase();
-            if (char.charCodeAt(0) >= 65 && char.charCodeAt(0) <= 90) {
-              const charKey = key[i_key].toUpperCase();
-              let charCode = (char.charCodeAt(0)-65) + (charKey.charCodeAt(0)-65);
-              if (Math.trunc(charCode / 25) > 0 ) {
-                charCode = (charCode % 25)-1;
-              }
-              strCrypto = strCrypto + String.fromCharCode(charCode + 65);
-              i_key++;
-              if (i_key > key.length-1) {
-                i_key = 0;
-              }
-            } else {
-              strCrypto = strCrypto + message[i];
+          const char = message[i].toUpperCase();
+          if (char.charCodeAt(0) >= 65 && char.charCodeAt(0) <= 90) {
+            const charKey = key[i_key].toUpperCase();
+            let charCode = (char.charCodeAt(0)-65) + (charKey.charCodeAt(0)-65);
+            if (charCode > 25 ) {
+              charCode = charCode - 26;
             }
+            strCrypto = strCrypto + String.fromCharCode(charCode + 65);
+            i_key++;
+            if (i_key > key.length-1) {
+              i_key = 0;
+            }
+          } else {
+            strCrypto = strCrypto + message[i];
+          }
         }
-        if (!this.lineCrypt) {
-          strCrypto.split().reverse.join('');
-        }
-        return (strCrypto);
+        return ( this.lineCrypt ? strCrypto : strCrypto.split('').reverse().join('') );
     }
 
 
@@ -668,27 +665,24 @@ class VigenereCipheringMachine {
         let i_key = 0;
 
         for (let i=0; i<message.length; i++) {
-            const charCrypto = message[i].toUpperCase();
-            if (charCrypto.charCodeAt(0) >= 65 && charCrypto.charCodeAt(0) <= 90) {
-              const charKey = key[i_key].toUpperCase();
-              let charCode = (charCrypto.charCodeAt(0)-65) - (charKey.charCodeAt(0)-65);
-              if ( charCode < 0 ) {
-                charCode = 26 + (charCrypto.charCodeAt(0)-65) - (charKey.charCodeAt(0)-65);
-              }
-              strDeCrypto = strDeCrypto + String.fromCharCode(charCode + 65);
-              i_key++;
-              if (i_key > key.length-1) {
-                i_key = 0;
-              }
-            } else {
-              strDeCrypto = strDeCrypto + message[i];
+          const charCrypto = message[i].toUpperCase();
+          if (charCrypto.charCodeAt(0) >= 65 && charCrypto.charCodeAt(0) <= 90) {
+            const charKey = key[i_key].toUpperCase();
+            let charCode = (charCrypto.charCodeAt(0)-65) - (charKey.charCodeAt(0)-65);
+            if ( charCode < 0 ) {
+              charCode = 26 + charCode;
             }
-        }
-        if (!this.lineCrypt) {
-          strDeCrypto.split().reverse.join('');
-        }
-        return (strDeCrypto);
-    }
+            strDeCrypto = strDeCrypto + String.fromCharCode(charCode + 65);
+            i_key++;
+            if (i_key > key.length-1) {
+              i_key = 0;
+            }
+          } else {
+            strDeCrypto = strDeCrypto + message[i];
+          }
+      }
+      return ( this.lineCrypt ? strDeCrypto : strDeCrypto.split('').reverse().join('') );
+    }  
 
 }
 directMachine = new VigenereCipheringMachine;
