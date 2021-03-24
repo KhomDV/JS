99d9b6e7021da7e5b84895734339e2faa4b2fc39
KhomDV <khomdv@mail.ru>
2021-03-24 12:14:59 +0300

    feat: End task basic-js

--- basic-js.js
+++ basic-js.js
@@ -747,30 +747,30 @@
 // ---
 //
 //---Solution---
-function getSeason(date) {
-  if ( !date ) return ('Unable to determine the time of year!');
-
-  if (date.hasOwnProperty("getMonth")) throw new Error();
- 
-  const month = date.getMonth();
-  if ( isNaN(month) ) throw new Error();
-
-  let season = "";
-  if(month >= 11 || month <= 1) {
-    season = 'winter';
-  }
-  if(month >= 2 && month <= 4) {
-    season = 'spring';
-  }
-  if(month >= 5 && month <= 7) {
-    season = 'summer';
-  }
-  if(month >= 8 && month <= 10) {
-    season = 'autumn';
-  } 
-
-  return (season);  
-}
+// function getSeason(date) {
+//   if ( !date ) return ('Unable to determine the time of year!');
+//
+//   if (date.hasOwnProperty("getMonth")) throw new Error();
+// 
+//   const month = date.getMonth();
+//   if ( isNaN(month) ) throw new Error();
+//
+//   let season = "";
+//   if(month >= 11 || month <= 1) {
+//     season = 'winter';
+//   }
+//   if(month >= 2 && month <= 4) {
+//     season = 'spring';
+//   }
+//   if(month >= 5 && month <= 7) {
+//     season = 'summer';
+//   }
+//   if(month >= 8 && month <= 10) {
+//     season = 'autumn';
+//   } 
+//
+//   return (season);  
+// }
 //
 //---Test---
 //let arrayTest = [
