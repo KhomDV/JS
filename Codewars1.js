5bc6f73ba29e9dbdadd49bb829a1424f5da3795d
KhomDV <KhomDV@mail.ru>
2021-03-25 23:16:51 +0300

    feat: solution End ( N-th Fibonacci )

--- Codewars1.js
+++ Codewars1.js
@@ -619,7 +619,15 @@
 //
 //---Solution---
 function nthFibo(n) {
-    // Return the n-th number in the Fibonacci Sequence
+  if (n <= 1) return 0;
+  let a = 0;
+  let b = 1;
+  for (let i = 3; i <= n; i++) {
+    let c = a + b;
+    a = b;
+    b = c;
+  }
+  return b;
 }
 //
 //---Test---
