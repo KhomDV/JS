a2699f6c5e307d11ed434c7987544ced40ed45ed
KhomDV <khomdv@mail.ru>
2021-03-26 14:29:11 +0300

    feat: Find The Parity Outlier

--- Codewars1.js
+++ Codewars1.js
@@ -786,43 +786,89 @@
 // ---
 //
 //---Solution---
-function divCon(x){
-    let result = 0;
-    let arr = [...new Set(x)];
-    for (i=0; i<arr.length; i++) {
-        if ( (x.filter((e) => e===arr[i] ).length % 2) !== 0 ) {
-            result = arr[i];
-        }    
-    }
-    return result;
-    //оригинально!!!
-    //return x.reduce((a, b) => a ^ b);
-}
+// function divCon(x){
+//     let result = 0;
+//     let arr = [...new Set(x)];
+//     for (i=0; i<arr.length; i++) {
+//         if ( (x.filter((e) => e===arr[i] ).length % 2) !== 0 ) {
+//             result = arr[i];
+//         }    
+//     }
+//     return result;
+//     //оригинально!!!
+//     //return x.reduce((a, b) => a ^ b);
+// }
 //
 //---Test---
-let arrayTest = [
-    [20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5], // 5);
-    [1,1,2,-2,5,2,4,4,-1,-2,5],// -1);
-    [20,1,1,2,2,3,3,5,5,4,20,4,5], //5);
-    [10], //10);
-    [1,1,1,1,1,1,10,1,1,1,1], //10);
-    [5,4,3,2,1,5,4,3,2,10,10] //1);
-]    
+// let arrayTest = [
+//     [20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5], // 5);
+//     [1,1,2,-2,5,2,4,4,-1,-2,5],// -1);
+//     [20,1,1,2,2,3,3,5,5,4,20,4,5], //5);
+//     [10], //10);
+//     [1,1,1,1,1,1,10,1,1,1,1], //10);
+//     [5,4,3,2,1,5,4,3,2,10,10] //1);
+// ]    
 //
 //---View solution---
-for (let i=0;i<arrayTest.length;i++) {
-    document.write(divCon(arrayTest[i]));
-    document.write(' | ');
-}
+// for (let i=0;i<arrayTest.length;i++) {
+//     document.write(divCon(arrayTest[i]));
+//     document.write(' | ');
+// }
 //
 //=== End ( Find the odd int ) ===
 
 
-
 //---------------------------------------------------------------------------------------------------
-// 
+// Find The Parity Outlier
 //6 kyu https://www.codewars.com/kata/find-the-parity-outlier
 //---------------------------------------------------------------------------------------------------
+//---Task---
+//### **Find The Parity Outlier**
+//You are given an array (which will have a length of at least 3, but could be very large) containing integers.
+//The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N.
+//Write a method that takes the array as an argument and returns this "outlier" N.
+//Examples
+//[2, 4, 0, 100, 4, 11, 2602, 36]
+//Should return: 11 (the only odd number)
+//[160, 3, 1719, 19, 11, 13, -21]
+//Should return: 160 (the only even number)
+//
+//### **Найдите выбросы по четности**
+//Вам предоставляется массив (который будет иметь длину не менее 3, но может быть очень большим),
+//содержащий целые числа. Массив либо полностью состоит из нечетных целых чисел,
+//либо полностью состоит из четных целых чисел, за исключением одного целого числа N.
+//Напишите метод, который принимает массив в качестве аргумента и возвращает этот «выброс» N.
+//Примеры
+//[2, 4, 0, 100, 4, 11, 2602, 36]
+//Should return: 11 (the only odd number)
+//[160, 3, 1719, 19, 11, 13, -21]
+//Should return: 160 (the only even number)
+// ---
+//
+//---Solution---
+function findOutlier(integers){
+    //your code here
+}
+//
+//---Test---
+// let arrayTest = [
+    [0, 1, 2], //1)
+    [1, 2, 3], //2)
+    [2,6,8,10,3], //3)
+    [0,0,3,0,0], //3)
+    [1,1,0,1,1] // 0)    
+]    
+//
+//---View solution---
+for (let i=0;i<arrayTest.length;i++) {
+    document.write(findOutlier(arrayTest[i]));
+    document.write(' | ');
+}
+//
+//=== End ( Find The Parity Outlier ) ===
+
+
+
 
 //---------------------------------------------------------------------------------------------------
 // 
