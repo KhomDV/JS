a29cf54be48aedb9b2d8e4576d33480e695780e8
KhomDV <KhomDV@mail.ru>
2021-04-11 20:42:35 +0300

    refactor: start 07-file-names

--- short-track.js
+++ short-track.js
@@ -289,41 +289,65 @@
 // ---
 //
 //---Solution---
-function isMAC48Address(n) {
+// function isMAC48Address(n) {
+//   macAddress = n.split("-");
+//   let lResult = true;
+//   for (let i=0; i < macAddress.length-1; i++) {
+//     // if (macAddress[i].length !== 2) {
+//     //   lResult = false;
+//     //   break;
+//     // }
+//     if (!/^[0-9A-F]{2,2}$/.test(macAddress[i])) {
+//       lResult = false;
+//       break;
+//     }
+//   }
+//   return lResult;
+// }
+//
+//---Test---
+// let arrayTest = [
+//   '00-1B-63-84-45-E6', //true);
+//   'Z1-1B-63-84-45-E6', //false);
+//   'not a MAC-48 address', //false);
+//   'FF-FF-FF-FF-FF-FF', //true);
+//   'G0-00-00-00-00-00' //false);
+// ]
+//---View solution---
+// for (let i=0;i<arrayTest.length;i++) {
+//   document.write(isMAC48Address(arrayTest[i]));
+//   document.write(' | ');
+// }
+//=== End ( 06-mac-address ) ===
 
-  //const reg = new RegExp('\\^(?=.*[a-z])(?=.*\\d)[a-z\\d]{4,19}$','i');
-  //const reg = new RegExp('[0-9A-F]-[0-9A-F]-[0-9A-F]-[0-9A-F]-[0-9A-F]','i');
-  //const reg = new RegExp('^[0-9A-F]{0,2}','i');
 
-//   const reg = new RegExp('^(?=.*[a-z])(?=.*\\d)[a-z\\d]{4,19}$','i');
-
-  macAddress = n.split("-");
-  let lResult = true;
-  for (let i=0; i < macAddress.length-1; i++) {
-    // if (macAddress[i].length !== 2) {
-    //   lResult = false;
-    //   break;
-    // }
-    if (!/^[0-9A-F]{2,2}$/.test(macAddress[i])) {
-      lResult = false;
-      break;
-    }
-  }
-  return lResult;
+//---------------------------------------------------------------------------------------------------
+// 07-file-names
+//---------------------------------------------------------------------------------------------------
+//---Task---
+//There's a list of file, since two files cannot have equal names,
+//the one which comes later will have a suffix (k),
+//where k is the smallest integer such that the found name is not used yet.
+//Return an array of names that will be given to the files.
+//@param {Array} names
+//@return {Array}
+//@example
+//For input ["file", "file", "image", "file(1)", "file"],
+//the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
+// ---
+//---Solution---
+function renameFiles(names) {
+  throw new Error('Not implemented');
 }
 //
 //---Test---
 let arrayTest = [
-  '00-1B-63-84-45-E6', //true);
-  'Z1-1B-63-84-45-E6', //false);
-  'not a MAC-48 address', //false);
-  'FF-FF-FF-FF-FF-FF', //true);
-  'G0-00-00-00-00-00' //false);
+  ["file", "file", "image", "file(1)", "file"] //["file", "file(1)", "image", "file(1)(1)", "file(2)"]
 ]
 //---View solution---
 for (let i=0;i<arrayTest.length;i++) {
   document.write(isMAC48Address(arrayTest[i]));
   document.write(' | ');
 }
-//=== End ( 06-mac-address ) ===
+//=== End ( 07-file-names ) ===
 
