b02ff87919a5a9492b628c636c38365c31b6286b
KhomDV <khomdv@mail.ru>
2021-05-24 16:53:03 +0300

    feat: solution task Fun with ES6 Classes #6 - Fake Files (Basic)

--- Codewars2.js
+++ Codewars2.js
@@ -326,23 +326,323 @@
 //---------------------------------------------------------------------------------------------------
 //---Task---
 //### **Fun with ES6 Classes #6 - Fake Files (Basic)**
+//Kata and Series Overview
+//You will be defining a class File (ES6 syntax please :) ) with the following properties and methods:
+//Properties
+//-fullName
+//-filename
+//-extension
+//Methods
+//-getContents()
+//-write(str)
+//-gets()
+//-getc()
+//Your File class should exhibit the following behaviour:
+//Constructor
+//Your constructor should accept two arguments in the following order: fullName and contents, where fullName is the full name of the file (including file extension)
+//and contents is the file contents.
+//An instance of your File class should exhibit the following behaviour:
+//fullName (property)
+//Should contain the full name of the file, including the file extension. Please note that the fullName property should be read-only, which means that attempts to reassign
+// fullName a new value should fail and it should retain its original value. For example:
+//var myFile = new File("hello.txt", "Hello World");
+//console.log(myFile.fullName); // hello.txt
+//myFile.fullName = "goodbye.txt"; // Reassignment should fail
+//console.log(myFile.fullName); // still "hello.txt"
+//filename (property)
+//Should contain the name of the file, excluding the file extension. Should also be read-only which means reassignment attempts should fail. E.g.
+//var file1 = new File("hello_world.txt", "Hello World");
+//console.log(file1.filename); // "hello_world"
+//file1.filename = "goodbye_world"; // Reassignment should fail
+//console.log(file1.filename); // still "hello_world"
+//var file2 = new File("class.phptester.php", "<?php /* Some PHP code here */ ?>");
+//console.log(file2.filename); // "class.phptester" - the filename should be correctly identified even if the filename itself contains the '.' character
+//extension (property)
+//Should contain the file extension. Read-only. E.g.
+//var fileWithComplexName = new File("alpha.beta.gamma.delta.txt", "alpha beta gamma delta");
+//console.log(fileWithComplexName.extension); // "txt"
+//fileWithComplexName.extension = "js"; // Reassignment should fail
+//console.log(fileWithComplexName.extension); // still "txt"
+//getContents (method)
+//Should return the contents of the file every time. E.g.
+//var myFile = new File("hello.txt", "Hello World\nHello World");
+//console.log(myFile.getContents()); // "Hello World\nHello World"
+//write (method)
+//Should accept exactly 1 argument str, the new content to be written to the file. The new content should be written on a new line on the file. For example:
+//var myFile = new File("example.txt", "");
+//console.log(myFile.getContents()); // ""
+//myFile.write("Line 1");
+//console.log(myFile.getContents()); // "Line 1"
+//myFile.write("Line 2");
+//console.log(myFile.getContents()); // "Line 1\nLine 2"
+//myFile.write("Line 3");
+//console.log(myFile.getContents()); // "Line 1\nLine 2\nLine 3"
+//gets (method)
+//Returns a line on the file, starting on Line 1. Successive calls to the method should return successive lines on the file.
+// If the number of calls exceeds the number of lines on the file, simple return undefined. E.g.:
+//var myFile = new File("example.txt", "Line 1\nLine 2\nLine 3\nLine 4\nLine 5");
+//console.log(myFile.gets()); // "Line 1"
+//console.log(myFile.gets()); // "Line 2"
+//console.log(myFile.gets()); // "Line 3"
+//console.log(myFile.gets()); // "Line 4"
+//console.log(myFile.gets()); // "Line 5"
+//console.log(myFile.gets()); // undefined
+//console.log(myFile.gets()); // undefined
+//console.log(myFile.gets()); // undefined
+//getc (method)
+//Should return a character on the file, starting from the first character. Successive calls should return successive characters on the file.
+// If the number of calls exceeds the number of characters on the file, it should simply return undefined. Example:
+//var myFile = new File("Lorem Ipsum.txt", "Lorem ipsum dolor sit amet, adispicing eu.");
+//console.log(myFile.getc()); // "L"
+//console.log(myFile.getc()); // "o"
+//console.log(myFile.getc()); // "r"
+//console.log(myFile.getc()); // "e"
+//console.log(myFile.getc()); // "m"
+//console.log(myFile.getc()); // " "
+//console.log(myFile.getc()); // "i"
+//console.log(myFile.getc()); // "p"
+// ... (many calls to myFile.getc())
+//console.log(myFile.getc()); // undefined (when number of calls exceeds character count)
+//Note regarding filenames
+//For the purposes of this Kata, all filenames used in this Kata will be valid filenames. Valid filenames are summarized as follows:
+//Contains a filename and extension (e.g. LICENSE.txt is a valid filename, LICENSE is not)
+//Filename contains only alphanumeric characters (both uppercase and lowercase), underscores, spaces and/or dots (e.g. index.php, class.phptester.php,
+// alpha beta.gamma_delta01.complicatedExample.txt are all valid filenames). Edge cases will not be considered (e.g. successive dots - Hello World..txt - will not appear in the test cases)
+//Extension contains only lowercase alphanumeric characters (e.g. txt, php, php3 are all valid)
+//Note regarding file content
+//All file content will be valid. In this Kata, valid file content may include alphanumeric characters (uppercase or lowercase), underscores, ordinary whitespace,
+// punctuation or mathematical symbols, in which each line will be separated from the next by a newline character ("\n").
+// There will not be tabs or other whitespace/newline characters other than "\s" (spacebar) or "\n" and the contents of any file will not start or end with a newline.
+// You may also assume that when the tests use the write(str) method, the string str will not contain newline characters itself.
 //
 //### ****
+//Задача
+//Вы будете определять class File(синтаксис ES6, пожалуйста :)) со следующими свойствами и методами:
+//Характеристики
+//-fullName
+//-filename
+//-extension
+//Методы
+//-getContents()
+//-write(str)
+//-gets()
+//-getc()
+//Ваш Fileкласс должен демонстрировать следующее поведение:
+//Конструктор
+//Конструктор должен принимать два аргумента в следующем порядке: fullNameи contents, где fullName- полное имя файла (включая расширение файла), а contents- содержимое файла.
+//Экземпляр вашего Fileкласса должен демонстрировать следующее поведение:
+//fullName (свойство)
+//Должен содержать полное имя файла, включая расширение файла . Обратите внимание, что fullNameсвойство должно быть доступно только для чтения, что означает,
+// что попытки переназначить fullNameновое значение должны завершиться неудачно, и оно должно сохранить свое исходное значение. Например:
+//var myFile = new File("hello.txt", "Hello World");
+//console.log(myFile.fullName); // hello.txt
+//myFile.fullName = "goodbye.txt"; // Reassignment should fail
+//console.log(myFile.fullName); // still "hello.txt"
+//имя файла (свойство)
+//Должен содержать имя файла без расширения файла . Также должен быть доступен только для чтения, что означает, что попытки переназначения не будут выполнены. Например
+//var file1 = new File("hello_world.txt", "Hello World");
+//console.log(file1.filename); // "hello_world"
+//file1.filename = "goodbye_world"; // Reassignment should fail
+//console.log(file1.filename); // still "hello_world"
+//var file2 = new File("class.phptester.php", "<?php /* Some PHP code here */ ?>");
+//console.log(file2.filename); // "class.phptester" - the filename should be correctly identified even if the filename itself contains the '.' character
+//расширение (свойство)
+//Должен содержать расширение файла. Только для чтения . Например
+//var fileWithComplexName = new File("alpha.beta.gamma.delta.txt", "alpha beta gamma delta");
+//console.log(fileWithComplexName.extension); // "txt"
+//fileWithComplexName.extension = "js"; // Reassignment should fail
+//console.log(fileWithComplexName.extension); // still "txt"
+//getContents (метод)
+//Должен каждый раз возвращать содержимое файла. Например
+//var myFile = new File("hello.txt", "Hello World\nHello World");
+//console.log(myFile.getContents()); // "Hello World\nHello World"
+//написать (метод)
+//Должен принимать ровно 1 аргумент str, новое содержимое будет записано в файл. Новый контент должен быть записан в новой строке файла. Например:
+//var myFile = new File("example.txt", "");
+//console.log(myFile.getContents()); // ""
+//myFile.write("Line 1");
+//console.log(myFile.getContents()); // "Line 1"
+//myFile.write("Line 2");
+//console.log(myFile.getContents()); // "Line 1\nLine 2"
+//myFile.write("Line 3");
+//console.log(myFile.getContents()); // "Line 1\nLine 2\nLine 3"
+//получает (метод)
+//Возвращает строку в файле, начиная со строки 1. Последовательные вызовы метода должны возвращать последовательные строки в файле.
+// Если количество вызовов превышает количество строк в файле, простой возврат undefined. Например:
+//var myFile = new File("example.txt", "Line 1\nLine 2\nLine 3\nLine 4\nLine 5");
+//console.log(myFile.gets()); // "Line 1"
+//console.log(myFile.gets()); // "Line 2"
+//console.log(myFile.gets()); // "Line 3"
+//console.log(myFile.gets()); // "Line 4"
+//console.log(myFile.gets()); // "Line 5"
+//console.log(myFile.gets()); // undefined
+//console.log(myFile.gets()); // undefined
+//console.log(myFile.gets()); // undefined
+//getc (метод)
+//Должен возвращать символ в файле, начиная с первого символа. Последовательные вызовы должны возвращать последовательные символы в файле.
+// Если количество вызовов превышает количество символов в файле, он должен просто вернуться undefined. Пример:
+//var myFile = new File("Lorem Ipsum.txt", "Lorem ipsum dolor sit amet, adispicing eu.");
+//console.log(myFile.getc()); // "L"
+//console.log(myFile.getc()); // "o"
+//console.log(myFile.getc()); // "r"
+//console.log(myFile.getc()); // "e"
+//console.log(myFile.getc()); // "m"
+//console.log(myFile.getc()); // " "
+//console.log(myFile.getc()); // "i"
+//console.log(myFile.getc()); // "p"
+//// ... (many calls to myFile.getc())
+//console.log(myFile.getc()); // undefined (when number of calls exceeds character count)
+//Примечание относительно имен файлов
+//Для целей этого Ката все имена файлов, используемые в этом Кате, будут действительными именами файлов. Допустимые имена файлов резюмируются следующим образом:
+//Содержит имя файла и расширение (например, LICENSE.txtдопустимое имя файла, LICENSEнет)
+//Имя файла содержит только буквенно - цифровые символы (прописные и строчные), подчеркивания, пробелов и / или точек (например index.php, class.phptester.php,
+// alpha beta.gamma_delta01.complicatedExample.txtвсе действительные имена файлов).
+// Пограничные случаи не будут рассматриваться (например, следующие друг за другом точки - Hello World..txt- не появятся в тестовых примерах)
+//Расширение содержит только строчные буквенно - цифровых символов (например txt, php, php3все действительны)
+//Примечание относительно содержимого файла
+//Все содержимое файла будет действительным. В этом Ката допустимое содержимое файла может включать буквенно-цифровые символы (в верхнем или нижнем регистре),
+// подчеркивания, обычные пробелы, знаки пунктуации или математические символы, в которых каждая строка будет отделена от следующей символом новой строки ( "\n").
+// Там будет не быть вкладки или другие , чем другие пробельные / перевод строки символов "\s"(пробел) или "\n"и содержимое любого файла будет не начинаться
+// или заканчиваться символом новой строки. Вы также можете предположить, что когда тесты используют этот write(str)метод, сама строка неstr будет содержать символов новой строки.
 //----------
 //
 //---Solution---
-//
+class File {
+  constructor(fullName, contents) {
+    this._fullName = fullName;
+    this.contents = contents;
+    this._filename = this._fullName.split('.').reverse().slice(1).reverse().join('.'); //edit
+    this._extension = this._fullName.split('.').pop();
+    this.row = 0;
+    this.col = 0;
+  }
+
+  get fullName() {
+    return this._fullName;
+  }
+  get filename() {
+    return this._filename;
+  }
+  get extension() {
+    return this._extension;
+  }
+  getContents() {
+    return this.contents;
+  }
+  write(str) {
+    if ( this.contents !== "" ) {
+      this.contents = this.contents + "\n";
+    }
+    this.contents = this.contents + str;
+  }
+  gets() {
+    return this.contents.split('\n')[this.row++];
+  }
+  getc() {
+    return this.contents[this.col++];
+  }
+}
 //---Test---
-// let arrayTest = [
-//     "Dermatoglyphics", //true );
-//     "isogram", //true );
-//     "aba", //false, "same chars may not be adjacent" );
-//     "moOse", //false, "same chars may not be same case" );
-//     "isIsogram", //false );
-//     "" //, true, "an empty string is a valid isogram" );
-// ]
-//
+// let arrayTest = []
 //---View solution---
+var myFile = new File("hello.txt", "Hello World");
+console.log(myFile.fullName); // hello.txt
+myFile.fullName = "goodbye.txt"; // Reassignment should fail
+console.log(myFile.fullName); // still "hello.txt"
+console.log(myFile.filename); // still "hello.txt"
+
+var file1 = new File("hello_world.txt", "Hello World");
+console.log(file1.filename); // "hello_world"
+file1.filename = "goodbye_world"; // Reassignment should fail
+console.log(file1.filename); // still "hello_world"
+
+var file2 = new File("class.phptester.php", "<?php /* Some PHP code here */ ?>");
+console.log(file2.filename); // "class.phptester" 
+
+var fileWithComplexName = new File("alpha.beta.gamma.delta.txt", "alpha beta gamma delta");
+console.log(fileWithComplexName.extension); // "txt"
+fileWithComplexName.extension = "js"; // Reassignment should fail
+console.log(fileWithComplexName.extension); // still "txt"
+
+var myFile = new File("hello.txt", "Hello World\nHello World");
+console.log(myFile.getContents()); // "Hello World\nHello World"
+
+var myFile = new File("example.txt", "");
+console.log(myFile.getContents()); // ""
+myFile.write("Line 1");
+console.log(myFile.getContents()); // "Line 1"
+myFile.write("Line 2");
+console.log(myFile.getContents()); // "Line 1\nLine 2"
+myFile.write("Line 3");
+console.log(myFile.getContents()); // "Line 1\nLine 2\nLine 3"
+
+var myFile = new File("example.txt", "Line 1\nLine 2\nLine 3\nLine 4\nLine 5");
+console.log(myFile.gets()); // "Line 1"
+console.log(myFile.gets()); // "Line 2"
+console.log(myFile.gets()); // "Line 3"
+console.log(myFile.gets()); // "Line 4"
+console.log(myFile.gets()); // "Line 5"
+console.log(myFile.gets()); // undefined
+console.log(myFile.gets()); // undefined
+console.log(myFile.gets()); // undefined
+
+var myFile = new File("Lorem Ipsum.txt", "Lorem ipsum dolor sit amet, adispicing eu.");
+console.log(myFile.getc()); // "L"
+console.log(myFile.getc()); // "o"
+console.log(myFile.getc()); // "r"
+console.log(myFile.getc()); // "e"
+console.log(myFile.getc()); // "m"
+console.log(myFile.getc()); // " "
+console.log(myFile.getc()); // "i"
+console.log(myFile.getc()); // "p"
+console.log(myFile.getc());
+console.log(myFile.getc());
+console.log(myFile.getc());
+console.log(myFile.getc());
+console.log(myFile.getc());
+console.log(myFile.getc());
+console.log(myFile.getc());
+console.log(myFile.getc());
+console.log(myFile.getc());
+console.log(myFile.getc());
+console.log(myFile.getc());
+console.log(myFile.getc());
+console.log(myFile.getc());
+console.log(myFile.getc());
+console.log(myFile.getc());
+console.log(myFile.getc());
+console.log(myFile.getc());
+console.log(myFile.getc());
+console.log(myFile.getc());
+console.log(myFile.getc());
+console.log(myFile.getc());
+console.log(myFile.getc());
+console.log(myFile.getc());
+console.log(myFile.getc());
+console.log(myFile.getc());
+console.log(myFile.getc());
+console.log(myFile.getc());
+console.log(myFile.getc());
+console.log(myFile.getc());
+console.log(myFile.getc());
+console.log(myFile.getc());
+console.log(myFile.getc());
+console.log(myFile.getc());
+console.log(myFile.getc());
+console.log(myFile.getc());
+console.log(myFile.getc());
+console.log(myFile.getc());
+console.log(myFile.getc());
+console.log(myFile.getc());
+console.log(myFile.getc());
+console.log(myFile.getc());
+console.log(myFile.getc());
+console.log(myFile.getc());
+console.log(myFile.getc());
+console.log(myFile.getc());
+//// ... (many calls to myFile.getc())
+//console.log(myFile.getc()); // undefined 
+//
 // for (let i=0;i<arrayTest.length;i++) {
 //     document.write(isIsogram(arrayTest[i]));
 //     document.write(' | ');
