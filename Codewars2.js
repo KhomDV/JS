1d908cdc16b93a54138a8af13b912191bb662173
KhomDV <KhomDV@mail.ru>
2021-05-29 01:28:02 +0300

    feat: solution task: Nuclear Missile Manager

--- Codewars2.js
+++ Codewars2.js
@@ -1277,35 +1277,35 @@
 
 
 //---------------------------------------------------------------------------------------------------
-// 
+// Nuclear Missile Manager
 // 6 kyu) https://www.codewars.com/kata/nuclear-missile-manager
 //---------------------------------------------------------------------------------------------------
 //---Task---
-//### ****
-//
-//### ****
+//### **Nuclear Missile Manager**
+//Our nuclear missile manager system is coded in Node.js. We are currently testing the system and it seems
+// that the launchAll function does not work as expected. It should launch 5 missiles each 1 second apart.
+// The current code tries to launch the missile #5 five times...
+//Can you fix this for us? You know, it's pretty critical code...
+//Note: There are 5 missiles labeled i which is a number in {0, 1, 2, 3, 4}.
+// The missile i should be launched after i seconds.
+//### **Управляющий ядерной ракетой**
+//Наша система управления ядерными ракетами написана на Node.js. В настоящее время мы тестируем систему,
+// и кажется, что launchAllфункция не работает должным образом. Он должен запускать 5 ракет с интервалом в 1 секунду.
+// Текущий код пытается запустить ракету №5 пять раз ...
+//Вы можете это исправить для нас? Знаете, это довольно критичный код ...
+//Примечание . Имеется 5 ракет, обозначенных цифрами iв {0, 1, 2, 3, 4}. Ракета iдолжна быть запущена через iсекунды.
 //----------
 //
 //---Solution---
-//
-//---Test---
-// let arrayTest = [
-//     "Dermatoglyphics", //true );
-//     "isogram", //true );
-//     "aba", //false, "same chars may not be adjacent" );
-//     "moOse", //false, "same chars may not be same case" );
-//     "isIsogram", //false );
-//     "" //, true, "an empty string is a valid isogram" );
-// ]
-//
-//---View solution---
-// for (let i=0;i<arrayTest.length;i++) {
-//     document.write(isIsogram(arrayTest[i]));
-//     document.write(' | ');
+// function launchAll(launchMissile) {
+//   for(var i = 0; i < 5; i++) {
+//     setTimeout(function() {
+//       launchMissile(i);
+//     }, i * 1000);
+//   }
 // }
-// 
-//=== End (  ) ===
-
+//---Test---
+//=== End ( Nuclear Missile Manager ) ===
 
 //===================================================================================================
 
