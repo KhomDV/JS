4538b5a8212f9405d3601255d9d354e9d6bcf61d
KhomDV <KhomDV@mail.ru>
2021-09-25 00:00:23 +0300

    feat: solution task Head, Tail, Init and Last

--- Codewars1.js
+++ Codewars1.js
@@ -555,6 +555,88 @@
 
 
 
+//---------------------------------------------------------------------------------------------------
+//  Arrays
+//---------------------------------------------------------------------------------------------------
+
+//---------------------------------------------------------------------------------------------------
+// Head, Tail, Init and Last
+//7 kyu https://www.codewars.com/kata/head-tail-init-and-last
+//---------------------------------------------------------------------------------------------------
+//---Task---
+//### **Head, Tail, Init and Last**
+//Haskell has some useful functions for dealing with lists:
+//$ ghci
+//GHCi, version 7.6.3: http://www.haskell.org/ghc/  :? for help
+//λ head [1,2,3,4,5]
+//1
+//λ tail [1,2,3,4,5]
+//[2,3,4,5]
+//λ init [1,2,3,4,5]
+//[1,2,3,4]
+//λ last [1,2,3,4,5]
+//5
+//Your job is to implement these functions in your given language. Make sure it doesn't edit the array; that would cause problems!
+//Here's a cheat sheet:
+//| HEAD | <----------- TAIL ------------> |
+//[  1,  2,  3,  4,  5,  6,  7,  8,  9,  10]
+//| <----------- INIT ------------> | LAST |
+//
+//head [x] = x
+//tail [x] = []
+//init [x] = []
+//last [x] = x
+//Here's how I expect the functions to be called in your language:
+//head([1,2,3,4,5]); => 1
+//tail([1,2,3,4,5]); => [2,3,4,5]
+//Most tests consist of 100 randomly generated arrays, each with four tests, one for each operation. There are 400 tests overall.
+//No empty arrays will be given. Haskell has QuickCheck tests
+//
+//### **Голова, Хвост, Начало и Последний**
+//В Haskell есть несколько полезных функций для работы со списками:
+//$ ghci
+//GHCi, version 7.6.3: http://www.haskell.org/ghc/  :? for help
+//λ head [1,2,3,4,5]
+//1
+//λ tail [1,2,3,4,5]
+//[2,3,4,5]
+//λ init [1,2,3,4,5]
+//[1,2,3,4]
+//λ last [1,2,3,4,5]
+//5
+//Ваша задача - реализовать эти функции на выбранном вами языке. Убедитесь, что он не редактирует массив;
+//это вызовет проблемы! Вот шпаргалка:
+//| HEAD | <----------- TAIL ------------> |
+//[  1,  2,  3,  4,  5,  6,  7,  8,  9,  10]
+//| <----------- INIT ------------> | LAST |
+//
+//head [x] = x
+//tail [x] = []
+//init [x] = []
+//last [x] = x
+//Вот как я ожидаю, что функции будут вызываться на вашем языке:
+//head([1,2,3,4,5]); => 1
+//tail([1,2,3,4,5]); => [2,3,4,5]
+//Большинство тестов состоит из 100 случайно сгенерированных массивов, каждый из которых содержит четыре теста,
+//по одному на каждую операцию. Всего 400 тестов. Никаких пустых массивов не будет. В Haskell есть тесты QuickCheck
+//---
+//
+//---Solution---
+const head = a => a[0];
+const tail = a => a.slice(1);
+const init = a => a.slice(0,a.length-1);
+const last = a => a[a.length-1];
+//
+//---Test---
+// console.log( solution(10) ); //, 23
+console.log( head([5,1]) ); //, 5 );
+console.log( tail([1]) ); //, [] );
+console.log( init([1,5,7,9]) ); //, [1,5,7] );
+console.log( last([7,2]) ); //, 2 );
+//
+//=== End ( Head, Tail, Init and Last ) ===
+
+
 
 
 
