0916b45a18f58c1e719620ec6ec13c58379d416c
KhomDV <KhomDV@mail.ru>
2021-09-24 20:26:18 +0300

    feat: solution task Handshake problem

--- Codewars1.js
+++ Codewars1.js
@@ -377,6 +377,49 @@
 //=== End ( Digits explosion ) ===
 
 
+//---------------------------------------------------------------------------------------------------
+// Handshake problem
+//6 kyu https://www.codewars.com/kata/handshake-problem
+//---------------------------------------------------------------------------------------------------
+//---Task---
+//### **Handshake problem**
+//Johnny is a farmer and he annually holds a beet farmers convention "Drop the beet".
+//Every year he takes photos of farmers handshaking. Johnny knows that no two farmers handshake more than once.
+//He also knows that some of the possible handshake combinations may not happen.
+//However, Johnny would like to know the minimal amount of people that participated this year just by counting
+//all the handshakes.
+//Help Johnny by writing a function, that takes the amount of handshakes and returns the minimal amount of people
+//needed to perform these handshakes (a pair of farmers handshake only once).
+//
+//### **Проблема с рукопожатием**
+//Джонни - фермер, и он ежегодно проводит съезд свекловодов «Брось свеклу».
+//Каждый год он фотографирует рукопожатие фермеров. Джонни знает, что никакие два фермера не пожимают руки более
+//одного раза. Он также знает, что некоторые из возможных комбинаций рукопожатий могут не произойти.
+//Однако Джонни хотел бы узнать минимальное количество людей, которые участвовали в этом году,
+//просто посчитав все рукопожатия.
+//Помогите Джонни, написав функцию, которая берет количество рукопожатий и возвращает минимальное количество людей,
+//необходимое для выполнения этих рукопожатий (пара фермеров пожимает руки только один раз).
+//---
+//
+//---Solution---
+function getParticipants(handshakes){
+  let people = 1;
+  let n = 0;
+  while (handshakes > n) {
+    n = n + people++;
+    //sum += people++;
+  }
+  return people;
+}
+//
+//---Test---
+console.log( getParticipants(0) ); //, 1)
+console.log( getParticipants(1) ); //, 2)
+console.log( getParticipants(3) ); //, 3)
+console.log( getParticipants(6) ); //, 4)
+console.log( getParticipants(7) ); //, 5)
+//
+//=== End ( Handshake problem ) ===
 
 
 
