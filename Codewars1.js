8640194adbb92c34cc2d73e2f327eb726d3f48e1
KhomDV <KhomDV@mail.ru>
2021-09-25 23:24:15 +0300

    feat: solution task Sorting by bits

--- Codewars1.js
+++ Codewars1.js
@@ -869,6 +869,69 @@
 //=== End ( Vasya - Clerk ) ===
 
 
+//---------------------------------------------------------------------------------------------------
+// Sorting by bits
+//6 kyu https://www.codewars.com/kata/sorting-by-bits
+//---------------------------------------------------------------------------------------------------
+//---Task---
+//### **Sorting by bits**
+//In this kata you're expected to sort an array of 32-bit integers in ascending order of the number of on bits they have.
+//E.g Given the array [7, 6, 15, 8]
+//7 has 3 on bits (000...0111)
+//6 has 2 on bits (000...0011)
+//15 has 4 on bits (000...1111)
+//8 has 1 on bit (000...1000)
+//So the array in sorted order would be [8, 6, 7, 15].
+//In cases where two numbers have the same number of bits, compare their real values instead.
+//E.g between 10 (...1010) and 12 (...1100), they both have the same number of on bits '2' but
+//the integer 10 is less than 12 so it comes first in sorted order.
+//Your task is to write the function sortBybit() that takes an array of integers and sort them as described above.
+//Note: Your function should modify the input rather than creating a new array.
+//sortByBit([3, 8, 3, 6, 5, 7, 9, 1]) // => [1, 8, 3, 3, 5, 6, 9, 7]
+//
+//### **Сортировка по битам**
+//В этом ката вы ожидали сортировать массив 32-битных целых чисел в порядке возрастания числа на бит у них есть.
+//Например, учитывая массив [7, 6, 15, 8]
+//7 имеет 3 бит (000 ... 0 111 )
+//6 имеет 2 двоичных разряда (000 ... 00 11 )
+//15 имеет 4 бит (000 ... 1111 )
+//8 имеет 1 бит (000 ... 1 000)
+//Таким образом, массив в отсортированном порядке будет [8, 6, 7, 15] .
+//В случаях, когда два числа имеют одинаковое количество битов, вместо этого сравните их реальные значения.
+//Например , в пределах от 10 (... 1010) и 12 (... 1100) , оба они имеют одинаковое число на бит « 2 » ,
+// но целое число 10 меньше , чем 12 , так что приходит первым в отсортированном порядке.
+//Ваша задача - написать функцию, sortBybit()которая принимает массив целых чисел и сортирует их, как описано выше.
+//Примечание. Ваша функция должна изменять ввод, а не создавать новый массив.
+//sortByBit([3, 8, 3, 6, 5, 7, 9, 1]) // => [1, 8, 3, 3, 5, 6, 9, 7]
+//---
+//
+//---Solution---
+// function sortByBit(arr) {
+//   return arr.sort(function(a,b) {
+//     a1 = String(a.toString(2)).split('').reduce((s,e)=>s + +e, 0);
+//     b1 = String(b.toString(2)).split('').reduce((s,e)=>s + +e, 0);
+//     if (a1<b1) return -1;
+//     if (a1===b1) {
+//       if (a<b) return -1;
+//       if (a===b) return 0;
+//       if (a>b) return 1;
+//     }
+//     if (a1>b1) return 1;
+//   });
+// }
+// //--2--
+// const sortByBit = arr => arr.sort((a, b) => a.toString(2).replace(/0/g, '') - b.toString(2).replace(/0/g, '') || a - b)
+//
+//---Test---
+// console.log( sortByBit([3, 8, 3, 6, 5, 7, 9, 1]) ); //[1, 8, 3, 3, 5, 6, 9, 7]
+// console.log( sortByBit([9,4,5,3,5,7,2,56,8,2,6,8,0]) ); //[0, 2, 2, 4, 8, 8, 3, 5, 5, 6, 9, 7, 56]
+// console.log( sortByBit([20,0,11]) ); //[0, 2, 2, 4, 8, 8, 3, 5, 5, 6, 9, 7, 56]
+// console.log( sortByBit([965997,973785,901104]) ); //[0, 2, 2, 4, 8, 8, 3, 5, 5, 6, 9, 7, 56]
+//
+//=== End ( Sorting by bits ) ===
+
+
+
 
 
 
