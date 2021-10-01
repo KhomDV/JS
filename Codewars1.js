47dc33e26c147d13a3e2acd715dad9f9b135d623
KhomDV <KhomDV@mail.ru>
2021-10-01 11:01:00 +0300

    feat: solution task Urban Dictionary

--- Codewars1.js
+++ Codewars1.js
@@ -1403,6 +1403,82 @@
 //
 //=== End ( Run-length encoding ) ===
 
+//---------------------------------------------------------------------------------------------------
+// Urban Dictionary - Solution
+//6 kyu https://www.codewars.com/kata/urban-dictionary
+//---------------------------------------------------------------------------------------------------
+//---Task---
+//### **Urban Dictionary**
+//Design a data structure that supports the following two operations:
+//addWord (or add_word) which adds a word,
+//search which searches a literal word or a regular expression string containing lowercase
+//letters "a-z" or "." where "." can represent any letter
+//You may assume that all given words contain only lowercase letters.
+//Examples
+//addWord("bad")
+//addWord("dad")
+//addWord("mad")
+//search("pad") === false
+//search("bad") === true
+//search(".ad") === true
+//search("b..") === true
+//Note: the data structure will be initialized multiple times during the tests!
+//
+//### **Словарь городского сленга**
+//Разработайте структуру данных, которая поддерживает следующие две операции:
+//addWord(или add_word) добавляющее слово,
+//searchкоторый ищет буквальное слово или строку регулярного выражения,
+//содержащую строчные буквы "a-z"или "."где "."может представлять любую букву
+//Вы можете считать, что все заданные слова содержат только строчные буквы.
+//Примеры
+//addWord("bad")
+//addWord("dad")
+//addWord("mad")
+//search("pad") === false
+//search("bad") === true
+//search(".ad") === true
+//search("b..") === true
+//Примечание: структура данных будет инициализирована несколько раз во время тестов!
+//---
+//---Solution---
+// var WordDictionary = function () {
+//   this.dictionary = [];
+// };
+
+// WordDictionary.prototype.addWord = function (word) {
+//   this.dictionary.push(word);
+// };
+
+// WordDictionary.prototype.search = function (word) {
+//   const reg = new RegExp('^'+word+'$','g');
+//   return this.dictionary.some(e => reg.test(e));
+// };
+//
+//---Test---
+// let wd = new WordDictionary();
+// wd.addWord('a');
+// wd.addWord('at');
+// wd.addWord('ate');
+// wd.addWord('ear');
+// console.log( wd.search('a') ); //, true);
+// console.log( wd.search('a.') ); //, true);
+// console.log( wd.search('a.e') ); //, true);
+// console.log( wd.search('b') ); //, false);
+// console.log( wd.search('e.') ); //, false);
+// console.log( wd.search('ea.') ); //, true);
+// console.log( wd.search('ea..') ); //, false);
+// wd.addWord('co');
+// wd.addWord('cod');
+// wd.addWord('code');
+// wd.addWord('codewars');
+// console.log( wd.search('........') ); //, true);
+// console.log( wd.search('c.o') ); //, false);
+// console.log( wd.search('cod.') ); //, true);
+// console.log( wd.search('c.o') ); //, false);
+// console.log( wd.search('co..w..s') ); //, true);
+// console.log( wd.search('co..w..') ); //, false);
+//
+//=== End ( Urban Dictionary ) ===
 
 
  //----------No solution
